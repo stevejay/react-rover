3a9eb3fa2306114d5491f118c23208a043e1433b
Steve Johns <steve@stevejay.net>
2021-12-02 20:35:57 +0000

    remove LabelBase

--- src/stories/Checkbox.tsx
+++ src/stories/Checkbox.tsx
@@ -1,7 +1,33 @@
 /** @jsxImportSource @emotion/react */
 import { forwardRef, InputHTMLAttributes } from 'react';
+import styled from '@emotion/styled';
 
-import { LabelBase } from './LabelBase';
+const StyledLabel = styled.label`
+  border: 1px solid white;
+  outline: none;
+  display: inline-block;
+  padding: 6px 12px;
+  border-radius: 5px;
+  text-align: center;
+  background: white;
+  color: #222428;
+  font-size: 14px;
+  line-height: 1.5em;
+  margin-right: 4px;
+  font-family: sans-serif;
+
+  &:hover {
+    border-color: #005a9c;
+    background: rgb(226, 239, 255);
+  }
+
+  &:focus-within {
+    border-width: 2px;
+    border-color: #005a9c;
+    background: rgb(226, 239, 255);
+    padding: 5px 11px;
+  }
+`;
 
 export type CheckboxProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange'> & {
   label: string;
@@ -15,7 +41,7 @@ export type CheckboxProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'onChang
 
 export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
   ({ label, checked, onChange, disabled = false, ...rest }, ref) => (
-    <LabelBase>
+    <StyledLabel>
       <input
         {...rest}
         type="checkbox"
@@ -25,6 +51,6 @@ export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
         onChange={(event) => onChange(event, event.target.checked)}
       />
       &nbsp;{label}
-    </LabelBase>
+    </StyledLabel>
   )
 );
