66e290e997ce24834e2ceb2a0a872ba05b1d1399
Steve Johns <steve@stevejay.net>
2021-12-06 17:06:47 +0000

    add tests

--- README.md
+++ README.md
@@ -5,6 +5,11 @@
 [![GitHub license](https://img.shields.io/github/license/stevejay/react-rover?style=for-the-badge)](https://github.com/stevejay/react-rover/blob/main/LICENSE)
 [![GitHub stars](https://img.shields.io/github/stars/stevejay/react-rover?style=for-the-badge)](https://github.com/stevejay/react-rover/stargazers)
 [![GitHub issues](https://img.shields.io/github/issues/stevejay/react-rover?style=for-the-badge)](https://github.com/stevejay/react-rover/issues)
+![Codacy grade](https://img.shields.io/codacy/grade/7933e4280d8642e9b503b45ff801c724?style=for-the-badge)
+![Code Climate technical debt](https://img.shields.io/codeclimate/tech-debt/stevejay/react-rover?style=for-the-badge)
+![Code Climate issues](https://img.shields.io/codeclimate/issues/stevejay/react-rover?style=for-the-badge)
+![Code Climate maintainability](https://img.shields.io/codeclimate/maintainability/stevejay/react-rover?style=for-the-badge)
+![CodeFactor Grade](https://img.shields.io/codefactor/grade/github/stevejay/react-rover/main?style=for-the-badge)
 
 ## Notes
 
--- src/keyDownTranslators.test.ts
+++ src/keyDownTranslators.test.ts
@@ -1,28 +1,74 @@
-import { mock } from 'jest-mock-extended';
+import { stub } from 'jest-auto-stub';
 
 import { extremesNavigation } from './keyDownTranslators';
 import { TabStop } from '.';
 
-describe('extremesNavigation', () => {
-  it('should navigate to the start', () => {
+describe('extremesNavigation when navigating to the start', () => {
+  it('should navigate to the start when all tab stops are enabled', () => {
     const mockEvent = { key: 'Home' } as unknown as React.KeyboardEvent<Element>;
     const tabStops: TabStop[] = [
-      { id: 'one', element: mock<HTMLButtonElement>() },
-      { id: 'two', element: mock<HTMLButtonElement>() },
-      { id: 'three', element: mock<HTMLButtonElement>() }
+      { id: 'one', element: stub<HTMLButtonElement>({ disabled: false }) },
+      { id: 'two', element: stub<HTMLButtonElement>({ disabled: false }) },
+      { id: 'three', element: stub<HTMLButtonElement>({ disabled: false }) }
     ];
-    const result = extremesNavigation(mockEvent, tabStops, tabStops[1]);
+    const result = extremesNavigation(mockEvent, tabStops, tabStops[2]);
     expect(result).toEqual({ newTabStopId: 'one' });
   });
 
-  it('should navigate to the end', () => {
-    const mockEvent = { key: 'End' } as unknown as React.KeyboardEvent<Element>;
+  it('should navigate to the first enabled tab stop when there are disabled tab stops', () => {
+    const mockEvent = { key: 'Home' } as unknown as React.KeyboardEvent<Element>;
     const tabStops: TabStop[] = [
-      { id: 'one', element: mock<HTMLButtonElement>() },
-      { id: 'two', element: mock<HTMLButtonElement>() },
-      { id: 'three', element: mock<HTMLButtonElement>() }
+      { id: 'one', element: stub<HTMLButtonElement>({ disabled: true }) },
+      { id: 'two', element: stub<HTMLButtonElement>({ disabled: false }) },
+      { id: 'three', element: stub<HTMLButtonElement>({ disabled: false }) }
+    ];
+    const result = extremesNavigation(mockEvent, tabStops, tabStops[2]);
+    expect(result).toEqual({ newTabStopId: 'two' });
+  });
+
+  it('should return the current tab stop when it is the first enabled tab stop', () => {
+    const mockEvent = { key: 'Home' } as unknown as React.KeyboardEvent<Element>;
+    const tabStops: TabStop[] = [
+      { id: 'one', element: stub<HTMLButtonElement>({ disabled: true }) },
+      { id: 'two', element: stub<HTMLButtonElement>({ disabled: false }) },
+      { id: 'three', element: stub<HTMLButtonElement>({ disabled: false }) }
     ];
     const result = extremesNavigation(mockEvent, tabStops, tabStops[1]);
+    expect(result).toEqual({ newTabStopId: 'two' });
+  });
+});
+
+describe('extremesNavigation when navigating to the end', () => {
+  it('should navigate to the end when all tab stops are enabled', () => {
+    const mockEvent = { key: 'End' } as unknown as React.KeyboardEvent<Element>;
+    const tabStops: TabStop[] = [
+      { id: 'one', element: stub<HTMLButtonElement>({ disabled: false }) },
+      { id: 'two', element: stub<HTMLButtonElement>({ disabled: false }) },
+      { id: 'three', element: stub<HTMLButtonElement>({ disabled: false }) }
+    ];
+    const result = extremesNavigation(mockEvent, tabStops, tabStops[0]);
     expect(result).toEqual({ newTabStopId: 'three' });
+  });
+
+  it('should navigate to the last enabled tab stop when there are disabled tab stops', () => {
+    const mockEvent = { key: 'End' } as unknown as React.KeyboardEvent<Element>;
+    const tabStops: TabStop[] = [
+      { id: 'one', element: stub<HTMLButtonElement>({ disabled: false }) },
+      { id: 'two', element: stub<HTMLButtonElement>({ disabled: false }) },
+      { id: 'three', element: stub<HTMLButtonElement>({ disabled: true }) }
+    ];
+    const result = extremesNavigation(mockEvent, tabStops, tabStops[0]);
+    expect(result).toEqual({ newTabStopId: 'two' });
+  });
+
+  it('should return the current tab stop when it is the last enabled tab stop', () => {
+    const mockEvent = { key: 'End' } as unknown as React.KeyboardEvent<Element>;
+    const tabStops: TabStop[] = [
+      { id: 'one', element: stub<HTMLButtonElement>({ disabled: false }) },
+      { id: 'two', element: stub<HTMLButtonElement>({ disabled: false }) },
+      { id: 'three', element: stub<HTMLButtonElement>({ disabled: true }) }
+    ];
+    const result = extremesNavigation(mockEvent, tabStops, tabStops[1]);
+    expect(result).toEqual({ newTabStopId: 'two' });
   });
 });
