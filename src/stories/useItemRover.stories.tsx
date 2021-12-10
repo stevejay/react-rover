b079be260ce0456742625225cdfb792b1941e395
Steve Johns <steve@stevejay.net>
2021-12-10 17:30:10 +0000

    improve grid examples

--- src/stories/useItemRover.stories.tsx
+++ src/stories/useItemRover.stories.tsx
@@ -63,10 +63,48 @@ const GridTemplate: Story<GridTemplateProps> = ({ columnsCount, itemCount }) => 
       <Button label="Focus before" />
       <Grid columnsCount={columnsCount} aria-label="Cells" {...getTabContainerProps()}>
         {items.map((item) => (
-          <Button key={item.id} label={item.label} {...getTabStopProps(item)} />
+          <Button key={item.id} disabled={item.id === '3'} label={item.label} {...getTabStopProps(item)} />
         ))}
       </Grid>
       <Button label="Focus after" />
+    </StackedLayout>
+  );
+};
+
+const DynamicGridTemplate: Story<void> = () => {
+  const [columnsCount, setColumnsCount] = useState(3);
+  const [enableButtonTwo, setEnableButtonTwo] = useState(false);
+  const [items] = useState<GridItem[]>(() => createItems(7));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const onTabStopChange = useCallback(() => action('current-item-changed'), []);
+  const { getTabContainerProps, getTabStopProps } = useItemRover(items, gridKeyDownTranslators, {
+    initialItem: items[2],
+    onTabStopChange,
+    columnsCount
+  });
+  return (
+    <StackedLayout>
+      <Button label="Focus before" />
+      <Grid columnsCount={columnsCount} aria-label="Cells" {...getTabContainerProps()}>
+        {items.map((item) => (
+          <Button
+            key={item.id}
+            label={item.label}
+            disabled={item.id === '2' ? !enableButtonTwo : false}
+            {...getTabStopProps(item)}
+          />
+        ))}
+      </Grid>
+      <div css={{ display: 'flex', gap: 16 }}>
+        <Button
+          label={`Change to ${columnsCount === 2 ? '3' : '2'} columns`}
+          onClick={() => setColumnsCount((state) => (state === 2 ? 3 : 2))}
+        />
+        <Button
+          label={enableButtonTwo ? 'Disable Button Two' : 'Enable Button Two'}
+          onClick={() => setEnableButtonTwo((state) => !state)}
+        />
+      </div>
     </StackedLayout>
   );
 };
@@ -107,6 +145,8 @@ SmallGrid.args = {
   columnsCount: 3,
   itemCount: 8
 };
+
+export const DynamicGrid = DynamicGridTemplate.bind({});
 
 export const Column = OneDimensionalTemplate.bind({});
 Column.args = {
