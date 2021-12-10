b9c088a6f3bc44414bb62726abb1fbff72491a68
Steve Johns <steve@stevejay.net>
2021-12-10 19:45:05 +0000

    tidy up

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,13 +3,6 @@ import { devices, PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   testDir: 'e2e-tests',
   outputDir: './e2e-tests-results',
-  //   timeout: 30000,
-  //   expect: {
-  //     timeout: 10000,
-  //     toMatchSnapshot: {
-  //       threshold: 0.3
-  //     }
-  //   },
   forbidOnly: !!process.env.CI,
   // Having to run Playwright in single-threaded mode locally
   // as some tests will time out without this:
@@ -24,15 +17,15 @@ const config: PlaywrightTestConfig = {
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] }
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] }
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] }
     }
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] }
-    // },
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] }
-    // }
   ]
 };
 export default config;
